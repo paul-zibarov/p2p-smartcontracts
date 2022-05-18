208bfb92cbdd56b943dc0c82ad3653286442f7ec
Paul Zibarov <paul.zibarov@gmail.com>
2022-05-18 22:50:08 +0300

    Implement BEP-20 token smart contract. Add Ownable smart contract.

---
+++ contracts/utils/Ownable.sol
@@ -0,0 +1,45 @@
+//SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+abstract contract Ownable {
+    address private _owner;
+    address private _newOwner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    constructor() {
+        _owner = msg.sender;
+        emit OwnershipTransferred(address(0), msg.sender);
+    }
+
+    function owner() public view virtual returns (address) {
+        return _owner;
+    }
+
+    function newOwner() public view virtual returns (address) {
+        return _newOwner;
+    }
+
+    modifier onlyOwner() {
+        require(owner() == msg.sender, "Ownable: caller is not the owner");
+        _;
+    }
+
+    function renounceOwnership() public virtual onlyOwner {
+        emit OwnershipTransferred(_owner, address(0));
+        _owner = address(0);
+    }
+
+    function transferOwnership(address transferOwner) public onlyOwner {
+        require(transferOwner != newOwner());
+        _newOwner = transferOwner;
+    }
+
+    function acceptOwnership() virtual public {
+        require(msg.sender == newOwner(), "Ownable: caller is not the new owner");
+        emit OwnershipTransferred(_owner, _newOwner);
+        _owner = _newOwner;
+        _newOwner = address(0);
+    }
+}
